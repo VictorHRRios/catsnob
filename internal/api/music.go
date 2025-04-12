5792fdd426ada1971d6fb038f5c088631257742c
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-04-12 13:49:17 -0700

    Structs for handling albums for later use

---
+++ internal/api/music.go
@@ -0,0 +1,52 @@
+package api
+
+type ArtistInfo struct {
+	Artists []struct {
+		IDArtist       string `json:"idArtist"`
+		StrArtist      string `json:"strArtist"`
+		StrLabel       string `json:"strLabel"`
+		IDLabel        string `json:"idLabel"`
+		IntFormedYear  string `json:"intFormedYear"`
+		IntBornYear    string `json:"intBornYear"`
+		StrGenre       string `json:"strGenre"`
+		StrBiographyEN string `json:"strBiographyEN"`
+		StrBiographyES string `json:"strBiographyES"`
+		IntMembers     string `json:"intMembers"`
+		StrCountry     string `json:"strCountry"`
+		StrCountryCode string `json:"strCountryCode"`
+		StrArtistThumb string `json:"strArtistThumb"`
+		StrArtistLogo  string `json:"strArtistLogo"`
+	} `json:"artists"`
+}
+
+type AlbumInfo struct {
+	Album []struct {
+		IDAlbum          string `json:"idAlbum"`
+		IDArtist         string `json:"idArtist"`
+		IDLabel          string `json:"idLabel"`
+		StrAlbum         string `json:"strAlbum"`
+		StrArtist        string `json:"strArtist"`
+		IntYearReleased  string `json:"intYearReleased"`
+		StrGenre         string `json:"strGenre"`
+		StrLabel         string `json:"strLabel"`
+		StrAlbumThumb    string `json:"strAlbumThumb"`
+		StrDescriptionEN string `json:"strDescriptionEN,omitempty"`
+		StrDescriptionES any    `json:"strDescriptionES"`
+		IntScore         string `json:"intScore"`
+		IntScoreVotes    string `json:"intScoreVotes"`
+	} `json:"album"`
+}
+
+type TrackInfo struct {
+	Track []struct {
+		IDTrack        string `json:"idTrack"`
+		IDAlbum        string `json:"idAlbum"`
+		IDArtist       string `json:"idArtist"`
+		StrTrack       string `json:"strTrack"`
+		StrAlbum       string `json:"strAlbum"`
+		StrArtist      string `json:"strArtist"`
+		IntDuration    string `json:"intDuration"`
+		StrGenre       string `json:"strGenre"`
+		IntTrackNumber string `json:"intTrackNumber"`
+	} `json:"track"`
+}
