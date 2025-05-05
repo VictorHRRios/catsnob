c9b650a63c17a9782921ca5016d354fbeef04e15
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-05-04 18:35:09 -0700

    add html parsed error handling to home page

--- internal/handlers/handler_home.go
+++ internal/handlers/handler_home.go
@@ -12,90 +12,75 @@ import (
 const layout = "templates/layout.html"
 
 func (cfg *ApiConfig) HandlerIndex(w http.ResponseWriter, r *http.Request, u *database.User) {
-	tmplPath := filepath.Join("templates", "home", "artists.html")
+	type returnVals struct {
+		Stylesheet *string
+		Artists    []database.Artist
+		User       *database.User
+		Error      error
+	}
 
+	tmplPath := filepath.Join("templates", "home", "artists.html")
 	tmpl, err := template.ParseFiles(layout, tmplPath)
 	if err != nil {
-		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		tmpl.Execute(w, returnVals{User: u, Error: err})
 		return
 	}
 
 	artists, err := cfg.Queries.GetTop12Artists(context.Background())
-	if err != nil {
-		http.Error(w, "Error fetching artists", http.StatusInternalServerError)
-		return
-	}
-
-	data := struct {
-		Stylesheet *string
-		Artists    []database.Artist
-		User       *database.User
-	}{
+	respBody := returnVals{
 		Stylesheet: nil,
 		Artists:    artists,
 		User:       u,
+		Error:      err,
 	}
 
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
-		return
-	}
-
+	tmpl.Execute(w, respBody)
 }
 
 func (cfg *ApiConfig) HandlerAlbums(w http.ResponseWriter, r *http.Request, u *database.User) {
+	type returnVals struct {
+		Stylesheet *string
+		Albums     []database.GetTop12AlbumsRow
+		User       *database.User
+		Error      error
+	}
 	tmplPath := filepath.Join("templates", "home", "albums.html")
-
 	tmpl, err := template.ParseFiles(layout, tmplPath)
 	if err != nil {
-		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		tmpl.Execute(w, returnVals{User: u, Error: err})
 		return
 	}
 
 	albums, err := cfg.Queries.GetTop12Albums(context.Background())
-
-	data := struct {
-		Stylesheet *string
-		Albums     []database.GetTop12AlbumsRow
-		User       *database.User
-	}{
+	respBody := returnVals{
 		Stylesheet: nil,
 		Albums:     albums,
 		User:       u,
+		Error:      err,
 	}
-
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
-		return
-	}
-
+	err = tmpl.Execute(w, respBody)
 }
 
 func (cfg *ApiConfig) HandlerTracks(w http.ResponseWriter, r *http.Request, u *database.User) {
+	type returnVals struct {
+		Stylesheet *string
+		Tracks     []database.GetTop12TracksRow
+		User       *database.User
+		Error      error
+	}
 	tmplPath := filepath.Join("templates", "home", "tracks.html")
-
 	tmpl, err := template.ParseFiles(layout, tmplPath)
 	if err != nil {
-		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		tmpl.Execute(w, returnVals{User: u, Error: err})
 		return
 	}
 
 	tracks, err := cfg.Queries.GetTop12Tracks(context.Background())
-	data := struct {
-		Stylesheet *string
-		Tracks     []database.GetTop12TracksRow
-		User       *database.User
-	}{
+	respBody := returnVals{
 		Stylesheet: nil,
 		Tracks:     tracks,
 		User:       u,
+		Error:      err,
 	}
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
-		return
-	}
-
+	tmpl.Execute(w, respBody)
 }
